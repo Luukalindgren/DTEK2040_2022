b97df43c495ddb4576dd95c043976916474b7d05
Luuka Lindgren <luslin@utu.fi>
2022-02-23 17:22:25 +0200

    1.4 tehty

--- exercise1/courses/src/index.js
+++ exercise1/courses/src/index.js
@@ -10,8 +10,7 @@ const Header = (props) => {
 
 const Part = (props) => {
   return (
-    <div>{props.content} {props.count}</div>
-    
+    <div>{props.content.name} {props.content.exercises}</div>
   )
 
 }
@@ -19,42 +18,43 @@ const Part = (props) => {
 const Contents = (props) => {
   return (
     <div>
-      <Part content={props.content1} count={props.count1} />
-      <Part content={props.content2} count={props.count2} />
-      <Part content={props.content3} count={props.count3} />
+      <Part content={props.parts[0]}  />
+      <Part content={props.parts[1]}  />
+      <Part content={props.parts[2]}  />
     </div>
   )
 }
 
 const Total = (props) => {
+  let sum = 0;
+  for(const element of props.parts) sum += element.exercises;
   return (
-    <div>Total {props.total} exercises</div>
+    <div>Total {sum} exercises</div>
   )
 }
 
 const App = () => {
   const course = 'Superadvanced web and mobile programming'
-  const part1 = {
-    name: 'Basics of React',
-    exercises: 8
-  }
-  const part2 = {
-    name: 'Using props',
-    exercises: 10
-  }
-  const part3 = {
-    name: 'Component states',
-    exercises: 12
-  }
-
+  const parts = [
+    {
+      name: 'Basics of React',
+      exercises: 8
+    },
+    {
+      name: 'Using props',
+      exercises: 10
+    },
+    {
+      name: 'Component states',
+      exercises: 12
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Contents content1={part1.name} count1={part1.exercises} 
-      content2={part2.name} count2={part2.exercises}
-      content3={part3.name} count3={part3.exercises}/>
-      <Total total={part1.exercises+part2.exercises+part3.exercises} />
+      <Contents parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
