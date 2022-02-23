94437425dbea6c1b63071be6ee7ed887ad356e0f
Luuka Lindgren <luslin@utu.fi>
2022-02-23 18:26:59 +0200

    1.6 tehty

--- exercise1/feedback/src/index.js
+++ exercise1/feedback/src/index.js
@@ -1,17 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      hyvä: 0,
+      neutraali: 0,
+      huono: 0
+    }
+    this.kasvataYhdella = this.kasvataYhdella.bind(this)
+  }
+
+  kasvataYhdella(tyyppi) {
+    if(tyyppi==="hyvä")this.setState({ hyvä: this.state.hyvä + 1 })
+    if(tyyppi==="neutraali")this.setState({ neutraali: this.state.neutraali + 1 })
+    if(tyyppi==="huono")this.setState({ huono: this.state.huono + 1 })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Anna palautetta:</h1>
+        
+        <div>
+          <button onClick={() => this.kasvataYhdella("hyvä")}> Hyvä </button>
+          <button onClick={() => this.kasvataYhdella("neutraali")}> Neutraali </button>
+          <button onClick={() => this.kasvataYhdella("huono")}> Huono </button>
+        </div>
+        <h1>Statistiikka:</h1>
+        <div>Hyvä {this.state.hyvä}</div>
+        <div>Neutraali {this.state.neutraali}</div>
+        <div>Huono {this.state.huono}</div>
+      </div>
+    )
+  }
+}
+  
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
