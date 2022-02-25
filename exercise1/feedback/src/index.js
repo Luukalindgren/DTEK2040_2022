5b11181c094533767a87026d431022622114def5
Luuka Lindgren <luslin@utu.fi>
2022-02-25 11:54:10 +0200

    1.8 ja 1.8 tehty

--- exercise1/feedback/src/index.js
+++ exercise1/feedback/src/index.js
@@ -2,7 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>{text}</button>
+)
 
+const Statistics = (props) => {
+  //Tarkistaa onko annettu palautetta
+  if(props.hyvä === 0 && props.neutraali === 0 && props.huono === 0) return <div>Ei yhtään palautetta</div>
+  
+  //Neutraalit (=0) ei ole positiivisia
+  let positiiviset = (props.hyvä/(props.hyvä + props.neutraali + props.huono)*100);
+
+  //hyvä = 1, neutraali = 0 ja huono = -1
+  let keskiarvo = (props.hyvä-props.huono)/(props.hyvä + props.neutraali + props.huono);
+
+  return (
+    <div>
+      <Statistic text="Hyvä" arvo={props.hyvä} > </Statistic>
+      <Statistic text="Neutraali" arvo={props.neutraali} />
+      <Statistic text="Huono" arvo={props.huono} />
+      <Statistic text="Keskiarvo" arvo={keskiarvo.toFixed(1)}/>
+      <Statistic text="Positiivisia" arvo={positiiviset.toFixed(1)} merkki="%"/>
+    </div>
+  )
+}
+
+const Statistic = (props) => {
+  return (
+    <p>{props.text} {props.arvo} {props.merkki}</p>
+  )
+}
 
 class App extends React.Component {
   constructor() {
@@ -16,42 +45,20 @@ class App extends React.Component {
 
   kasvataYhdella = (tyyppi) => {
     if(tyyppi==="hyvä")this.setState({ hyvä: this.state.hyvä + 1 })
-    if(tyyppi==="neutraali")this.setState({ neutraali: this.state.neutraali + 1 })
+    if(tyyppi==="neutraali")this.setState({ neutraali:this.state.neutraali + 1 })
     if(tyyppi==="huono")this.setState({ huono: this.state.huono + 1 })
-  }
-
-  //hyvä = 1, neutraali = 0 ja huono = -1
-  keskiarvo = () => {
-    if(this.state.hyvä === 0 & this.state.neutraali === 0 & this.state.huono === 0) return "0";
-    let sum = this.state.hyvä-this.state.huono;
-    let maara = this.state.hyvä + this.state.neutraali + this.state.huono;
-    return (sum/maara).toFixed(1);
-  }
-
-  //Neutraalit (=0) ei ole positiivisia
-  positiivisia = () => {
-    if(this.state.hyvä === 0 & this.state.neutraali === 0 & this.state.huono === 0) return "0";
-    let posit = this.state.hyvä;
-    let maara = this.state.hyvä + this.state.neutraali + this.state.huono;
-    return (posit/maara*100).toFixed(1);
   }
 
   render() {
     return (
       <div>
-        <div>
-          <h1>Anna palautetta:</h1>
-          <button onClick={() => this.kasvataYhdella("hyvä")}> Hyvä </button>
-          <button onClick={() => this.kasvataYhdella("neutraali")}> Neutraali </button>
-          <button onClick={() => this.kasvataYhdella("huono")}> Huono </button>
-        </div>
+        <h1>Anna palautetta:</h1>
+        <Button handleClick={() => this.kasvataYhdella("hyvä")} text="Hyvä"/>
+        <Button handleClick={() => this.kasvataYhdella("neutraali")} text="Neutraali"/>
+        <Button handleClick={() => this.kasvataYhdella("huono")} text="Huono"/>
         <div>
           <h1>Statistiikka:</h1>
-          <p>Hyvä {this.state.hyvä}</p>
-          <p>Neutraali {this.state.neutraali}</p>
-          <p>Huono {this.state.huono}</p>
-          <p>Keskiarvo {this.keskiarvo()}</p>
-          <p>Positiivisia {this.positiivisia()}%</p>
+          <Statistics hyvä={this.state.hyvä} neutraali={this.state.neutraali} huono={this.state.huono}/>
         </div>
       </div>
     )
