83796448346783521f820006b795ae9c173f8625
Luuka Lindgren <luslin@utu.fi>
2022-04-06 12:14:29 +0300

    3.2 tehty

--- exercise3/phonebook-back/src/index.js
+++ exercise3/phonebook-back/src/index.js
@@ -30,6 +30,16 @@ app.get('/', (req, res) => {
 
 app.get('/api/persons', (req, res) => {
     res.json(persons)
+}
+)
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
 })
 
 const PORT = 3001
